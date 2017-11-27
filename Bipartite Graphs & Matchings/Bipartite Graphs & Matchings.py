f74f76bf7d8cbafe3cd59aeed7bf265d655ebba5
Pierce Ortlieb <po@PO-MacBook.local>
2017-11-27 12:03:22 -0500

    Created initial code skeleton

---
+++ Bipartite Graphs & Matchings/Bipartite Graphs & Matchings.py
@@ -0,0 +1,16 @@
+# Author: Pierce Ortlieb
+# Title: Bipartitie Graphs & Matchings
+# Date: 11/27/17
+
+def power(list):
+    print("power")
+
+def partite_sets(graph):
+    print("partite")
+
+def is_perfect(graph):
+    print("is perfect")
+
+def is_bipartite(graph):
+    print("is bipartite")
+
