dc3f376c9baa41bfc9176fe0b5fd214d6b5ab362
Pierce Ortlieb <po@PO-MacBook.local>
2017-11-29 15:17:26 -0500

    Power set function is working properly.

--- Bipartite Graphs & Matchings/Bipartite Graphs & Matchings.py
+++ Bipartite Graphs & Matchings/Bipartite Graphs & Matchings.py
@@ -2,20 +2,26 @@
 # Title: Bipartitie Graphs & Matchings
 # Date: 11/29/17
 
-# Find the power set of a given list
-def power(list):
-    set = list
-    sets = []
+# Find the power set of a given set
+def power(set):
+    power_set = []
     # Iterate over our initial list
     for item in set:
-        # Iterate over each set in sets
-        for set in sets:
-            sets.append(set + item)
-        sets.append(item)
-    sets.append(" ")
+        # Iterate over each sub set of our in-progress powerset
+        for sub_set in power_set:
+                # Check to make sure we dont end up with something like ABB or BCCC
+                if item not in sub_set:
+                    power_set.append(list(sub_set) + list(item))
+        # Append our single values such as A, B, C
+        power_set.append(list(item))
+    # Sort our power set by length
+    power_set.sort(key=len)
+    # Empty set is always part of a power set
+    power_set.insert(0, list(' '))
+    return power_set
 
-temp = ["A", "B"]
-power(temp)
+power_temp = ['A', 'B']
+power(power_temp)
 
 
 # def partite_sets(graph):
